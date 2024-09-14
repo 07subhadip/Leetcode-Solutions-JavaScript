240ce84dc66e81f2d59bf10eef738be00f397331
07subhadip <subhadiphensh31102000@gmail.com>
2024-09-14 17:22:44 +0530

    leetcode daily problem solved

---
+++ 2419_Longest_Subarray_With_Maximum_Bitwise_AND.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestSubarray = function(nums) {
+    let mx = Math.max(...nums);
+    
+    let ans = 0, cnt = 0;
+    
+    for (let v of nums) {
+        if (v === mx) {
+            cnt++;
+            ans = Math.max(ans, cnt);
+        } else {
+            cnt = 0;
+        }
+    }
+    
+    return ans;
+};
