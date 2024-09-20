63fac2ae3ace972106c3be7dd971762d0c408c12
07subhadip <subhadiphensh31102000@gmail.com>
2024-09-20 10:57:32 +0530

    leetcode daily problem solved

---
+++ 214_Shortest_Palindrome.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var shortestPalindrome = function(s) {
+    const rev = s.split('').reverse().join('');
+    const t = s + '#' + rev + '$';
+    const n = t.length;
+    const next = Array(n).fill(0);
+    next[0] = -1;
+    
+    for (let i = 2, j = 0; i < n; ) {
+        if (t[i - 1] === t[j]) {
+            next[i++] = ++j;
+        } else if (j > 0) {
+            j = next[j];
+        } else {
+            next[i++] = 0;
+        }
+    }
+    
+    return rev.slice(0, -next[n - 1]) + s;
+};
