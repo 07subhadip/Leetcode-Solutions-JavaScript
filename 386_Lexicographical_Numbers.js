3fa609e15cc3c1b4790170e5b55a8bd76a50fdef
07subhadip <subhadiphensh31102000@gmail.com>
2024-09-21 08:39:40 +0530

    leetcode daily problem solved

---
+++ 386_Lexicographical_Numbers.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var lexicalOrder = function(n) {
+    const ans = [];
+    let v = 1;
+    for (let i = 0; i < n; ++i) {
+        ans.push(v);
+        if (v * 10 <= n) {
+            v *= 10;
+        } else {
+            while (v % 10 === 9 || v === n) {
+                v = Math.floor(v / 10);
+            }
+            ++v;
+        }
+    }
+    return ans;
+};
