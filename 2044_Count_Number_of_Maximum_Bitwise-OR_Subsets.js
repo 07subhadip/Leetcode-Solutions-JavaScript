c4442bf2205278eac4c28341449a4271c320ceaa
07subhadip <subhadiphensh31102000@gmail.com>
2024-10-18 09:04:39 +0530

    leetcode daily problem solved beats 100%

---
+++ 2044_Count_Number_of_Maximum_Bitwise-OR_Subsets.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countMaxOrSubsets = function(nums) {
+    let n = nums.length;
+    let max = 0;
+    for (let i = 0; i < n; i++) {
+        max |= nums[i];
+    }
+    let ans = 0;
+    function dfs(pre, depth) {
+        if (depth === n) {
+            if (pre === max) ++ans;
+            return;
+        }
+        dfs(pre, depth + 1);
+        dfs(pre | nums[depth], depth + 1);
+    }
+    dfs(0, 0);
+    return ans;
+};
