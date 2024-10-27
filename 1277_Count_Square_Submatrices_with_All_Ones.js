f89b5b4c0a8d613964ca14418a210d54bd10358b
07subhadip <subhadiphensh31102000@gmail.com>
2024-10-27 11:51:07 +0530

    leetcode daily problem solved

---
+++ 1277_Count_Square_Submatrices_with_All_Ones.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var countSquares = function(matrix) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+    let ans = 0;
+    const f = Array.from({ length: m }, () => Array(n).fill(0));
+    
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] === 0) continue;
+            if (i === 0 || j === 0) {
+                f[i][j] = 1;
+            } else {
+                f[i][j] = Math.min(f[i - 1][j - 1], Math.min(f[i - 1][j], f[i][j - 1])) + 1;
+            }
+            ans += f[i][j];
+        }
+    }
+    
+    return ans;
+};
