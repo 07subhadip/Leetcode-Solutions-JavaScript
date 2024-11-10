f5e9349993f6172300b581120f0cb440d04d26ff
07subhadip <subhadiphensh31102000@gmail.com>
2024-11-10 10:20:05 +0530

    leetcode daily problem solved

---
+++ 3097_Shortest_Subarray_With_OR_at_Least_K_II.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minimumSubarrayLength = function (nums, k) {
+    const n = nums.length;
+    let ans = n + 1;
+    const cnt = new Array(32).fill(0);
+    for (let i = 0, j = 0, s = 0; j < n; ++j) {
+        s |= nums[j];
+        for (let h = 0; h < 32; ++h) {
+            if (((nums[j] >> h) & 1) === 1) {
+                ++cnt[h];
+            }
+        }
+        for (; s >= k && i <= j; ++i) {
+            ans = Math.min(ans, j - i + 1);
+            for (let h = 0; h < 32; ++h) {
+                if (((nums[i] >> h) & 1) === 1 && --cnt[h] === 0) {
+                    s ^= 1 << h;
+                }
+            }
+        }
+    }
+    return ans === n + 1 ? -1 : ans;
+}
