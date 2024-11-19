aa342d1b7f1a3676706de32648ad6983345c541c
07subhadip <subhadiphensh31102000@gmail.com>
2024-11-19 09:35:13 +0530

    leetcode daily problem sovled

---
+++ 2461_Maximum_Sum_of_Distinct_Subarrays_With_Length_K.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maximumSubarraySum = function (nums, k) {
+    const n = nums.length;
+    const cnt = new Map();
+    let s = 0;
+
+    for (let i = 0; i < k; ++i) {
+        cnt.set(nums[i], (cnt.get(nums[i]) || 0) + 1);
+        s += nums[i];
+    }
+
+    let ans = cnt.size === k ? s : 0;
+
+    for (let i = k; i < n; ++i) {
+        cnt.set(nums[i], (cnt.get(nums[i]) || 0) + 1);
+        cnt.set(nums[i - k], cnt.get(nums[i - k]) - 1);
+        if (cnt.get(nums[i - k]) === 0) {
+            cnt.delete(nums[i - k]);
+        }
+        s += nums[i] - nums[i - k];
+        if (cnt.size === k) {
+            ans = Math.max(ans, s);
+        }
+    }
+
+    return ans;
+};
