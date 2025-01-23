5a008d36e47735f4befb8537ab3a323fe1f89e33
07subhadip <subhadiphensh31102000@gmail.com>
2025-01-23 10:27:01 +0530

    add solution for 1267. Count Servers that Communicate

---
+++ 1267_Count_Servers_that_Communicate.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var countServers = function(grid) {
+    const m = grid.length;
+    const n = grid[0].length;
+    const row = new Array(m).fill(0);
+    const col = new Array(n).fill(0);
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 1) {
+                row[i]++;
+                col[j]++;
+            }
+        }
+    }
+    let ans = 0;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 1 && (row[i] > 1 || col[j] > 1)) {
+                ans++;
+            }
+        }
+    }
+    return ans;
+};
