4efa76adba85d040f5f9e87adc82c49c28326cd3
07subhadip <subhadiphensh31102000@gmail.com>
2025-02-10 19:12:17 +0530

    refactor: improve formatting in clearDigits function

--- 3174_Clear_Digits.js
+++ 3174_Clear_Digits.js
@@ -2,15 +2,15 @@
  * @param {string} s
  * @return {string}
  */
-var clearDigits = function(s) {
+var clearDigits = function (s) {
     while (/\d/.test(s)) {
         let idx = s.search(/\d/);
-        
+
         let leftIdx = idx - 1;
         while (leftIdx >= 0 && /\d/.test(s[leftIdx])) {
             leftIdx--;
         }
-        
+
         if (leftIdx >= 0) {
             s = s.substring(0, leftIdx) + s.substring(leftIdx + 1, idx) + s.substring(idx + 1);
         } else {
