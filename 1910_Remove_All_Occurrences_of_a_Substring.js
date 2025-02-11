27752f0dd561bafd0be85264b8c9bdd532048142
07subhadip <subhadiphensh31102000@gmail.com>
2025-02-11 10:15:22 +0530

    add solution for 1910. Remove All Occurrences of a Substring

---
+++ 1910_Remove_All_Occurrences_of_a_Substring.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @param {string} part
+ * @return {string}
+ */
+var removeOccurrences = function(s, part) {
+    const stack = [];
+    const partLen = part.length;
+    
+    for (let i = 0; i < s.length; i++) {
+        stack.push(s[i]);
+        
+        if (stack.length >= partLen) {
+            let match = true;
+            for (let j = 0; j < partLen; j++) {
+                if (stack[stack.length - partLen + j] !== part[j]) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                for (let j = 0; j < partLen; j++) {
+                    stack.pop();
+                }
+            }
+        }
+    }
+    
+    return stack.join('');
+};
