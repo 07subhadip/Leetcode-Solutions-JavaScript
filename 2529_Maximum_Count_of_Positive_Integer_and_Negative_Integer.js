79f52a4d09461a26af07bf3354e5e754e2747676
07subhadip <subhadiphensh31102000@gmail.com>
2025-03-12 10:08:09 +0530

    refactor binarySearch function for improved readability

--- 2529_Maximum_Count_of_Positive_Integer_and_Negative_Integer.js
+++ 2529_Maximum_Count_of_Positive_Integer_and_Negative_Integer.js
@@ -14,10 +14,12 @@ var maximumCount = function (nums) {
 
 const binarySearch = (arr, target) => {
     let left = 0, right = arr.length;
+
     while (left < right) {
         let mid = Math.floor((left + right) / 2);
         if (arr[mid] < target) left = mid + 1;
         else right = mid;
     }
+    
     return left;
 }
