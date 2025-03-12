600d26456b51812d0bc06a46d7ad0961617e46b3
07subhadip <subhadiphensh31102000@gmail.com>
2025-03-12 10:07:48 +0530

    add solution for 2529. Maximum Count of Positive Integer and Negative Integer

---
+++ 2529_Maximum_Count_of_Positive_Integer_and_Negative_Integer.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumCount = function (nums) {
+    let n = nums.length;
+
+    let negCount = binarySearch(nums, 0);
+
+    let posCount = n - binarySearch(nums, 1);
+
+    return Math.max(negCount, posCount);
+};
+
+const binarySearch = (arr, target) => {
+    let left = 0, right = arr.length;
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        if (arr[mid] < target) left = mid + 1;
+        else right = mid;
+    }
+    return left;
+}
