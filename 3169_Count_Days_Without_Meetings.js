e5d7c19991417bd9c43df8cc1b84de606d4175fa
07subhadip <subhadiphensh31102000@gmail.com>
2025-03-24 12:08:07 +0530

    add optimized solution for 3169. Count Days Without Meetings

--- 3169_Count_Days_Without_Meetings.js
+++ 3169_Count_Days_Without_Meetings.js
@@ -34,3 +34,49 @@ var countDays = function (days, meetings) {
 
     return days - totalBusyDays;
 };
+
+//Solution 2 : Beats 58.06% of Users on Runtime
+
+/**
+ * @param {number} days
+ * @param {number[][]} meetings
+ * @return {number}
+ */
+var countDays = function (days, meetings) {
+    meetings.sort((a, b) => a[0] - b[0]);
+
+    let merged = [];
+
+    for (let meeting of meetings) {
+        if (merged.length === 0 || merged[merged.length - 1][1] < meeting[0] - 1) {
+            merged.push([meeting[0], meeting[1]]);
+        } else {
+            merged[merged.length - 1][1] = Math.max(merged[merged.length - 1][1], meeting[1]);
+        }
+    }
+
+    let freeDays = 0;
+
+    if (merged.length > 0 && merged[0][0] > 1) {
+        freeDays += merged[0][0] - 1;
+    }
+
+    for (let i = 1; i < merged.length; i++) {
+        let prevEnd = merged[i - 1][1];
+        let nextStart = merged[i][0];
+
+        if (prevEnd < nextStart - 1) {
+            freeDays += nextStart - prevEnd - 1;
+        }
+    }
+
+    if (merged.length > 0 && merged[merged.length - 1][1] < days) {
+        freeDays += days - merged[merged.length - 1][1];
+    }
+
+    if (merged.length === 0) {
+        freeDays = days;
+    }
+
+    return freeDays;
+};
