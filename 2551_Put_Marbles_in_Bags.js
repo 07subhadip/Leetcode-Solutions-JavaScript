b94ec4f066e6ad68fb0525c017d6b8fe9ce55f05
07subhadip <subhadiphensh31102000@gmail.com>
2025-03-31 09:37:43 +0530

    add alternative solution for 2551. Put Marbles in Bags

--- 2551_Put_Marbles_in_Bags.js
+++ 2551_Put_Marbles_in_Bags.js
@@ -26,3 +26,39 @@ var putMarbles = function (weights, k) {
 
     return maxSum - minSum;
 };
+
+// Solution 2 : Not optimized but one of the solutions
+
+/**
+ * @param {number[]} weights
+ * @param {number} k
+ * @return {number}
+ */
+var putMarbles = function (weights, k) {
+    const n = weights.length;
+
+    if (k === 1 || k === n) {
+        return 0;
+    }
+
+    const adjacentSums = [];
+
+    for (let i = 0; i < n - 1; i++) {
+        adjacentSums.push(weights[i] + weights[i + 1]);
+    }
+
+    adjacentSums.sort((a, b) => a - b);
+    let minSum = 0;
+
+    for (let i = 0; i < k - 1; i++) {
+        minSum += adjacentSums[i];
+    }
+
+    let maxSum = 0;
+
+    for (let i = adjacentSums.length - (k - 1); i < adjacentSums.length; i++) {
+        maxSum += adjacentSums[i];
+    }
+
+    return maxSum - minSum;
+};
