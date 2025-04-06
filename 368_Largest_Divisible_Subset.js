d94a98a7d43400b12986fbfaded104da3b75b3d5
07subhadip <subhadiphensh31102000@gmail.com>
2025-04-06 08:44:39 +0530

    add second solution for 368. Largest Divisible Subset

--- 368_Largest_Divisible_Subset.js
+++ 368_Largest_Divisible_Subset.js
@@ -34,3 +34,34 @@ var largestDivisibleSubset = function (nums) {
 
     return res;
 };
+
+
+// Solution 2: Beats 100% of JS submissions
+
+function largestDivisibleSubset(nums) {
+    nums = new Uint32Array(nums).sort();
+    const n = nums.length;
+    const dp = new Uint16Array(n).fill(1);
+    const arr = new Uint16Array(n).fill(10000);
+    let idx = 0;
+
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] % nums[j]) continue;
+
+            if (dp[j] + 1 <= dp[i]) continue;
+
+            dp[i] = dp[j] + 1, arr[i] = j;
+        }
+
+        if (dp[i] > dp[idx]) idx = i;
+    }
+    const res = [];
+
+    while (idx < 10000) {
+        res.push(nums[idx]);
+        idx = arr[idx];
+    }
+
+    return res.reverse();
+}
