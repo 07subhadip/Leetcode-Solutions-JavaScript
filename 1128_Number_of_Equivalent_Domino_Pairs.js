c20f8da9339447c954393c73767d5ca529da3b78
07subhadip <subhadiphensh31102000@gmail.com>
2025-05-04 09:45:06 +0530

    Add second solution for 1128.Number of Equivalent Domino Pairs with improved runtime efficiency

--- 1128_Number_of_Equivalent_Domino_Pairs.js
+++ 1128_Number_of_Equivalent_Domino_Pairs.js
@@ -20,3 +20,26 @@ var numEquivDominoPairs = function (dominoes) {
 
     return count
 };
+
+// Solution 2 :  Beats 98.39% users on runtime
+
+/**
+ * @param {number[][]} dominoes
+ * @return {number}
+ */
+var numEquivDominoPairs = function (dominoes) {
+    const counts = new Array(100).fill(0)
+    let ans = 0
+
+    for (let i = 0; i < dominoes.length; ++i) {
+        const [a, b] = dominoes[i]
+        const low = a < b ? a : b
+        const high = a < b ? b : a
+        const key = low * 10 + high
+
+        ans += counts[key]
+        counts[key]++
+    }
+
+    return ans
+};
