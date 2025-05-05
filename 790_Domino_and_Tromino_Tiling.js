efeb88ecd91ddec5130d8db685cd61d91bc4ebd2
07subhadip <subhadiphensh31102000@gmail.com>
2025-05-05 11:24:38 +0530

    Add second solution for 790.Domino and Tromino Tiling with improved time complexity

--- 790_Domino_and_Tromino_Tiling.js
+++ 790_Domino_and_Tromino_Tiling.js
@@ -17,3 +17,28 @@ var numTilings = function(n) {
 
     return f[0]
 };
+
+//Solution 2 : Reduced Time of Execution
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTilings = function(n) {
+    if(n === 1) return 1
+    if(n === 2) return 2
+    if(n === 3) return 5
+
+    const MOD = 1e9 + 7
+    const dp = new Array(n + 1).fill(0)
+
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 5
+
+    for(let i = 4; i <= n ; i++){
+        dp[i] = (2 * dp[i - 1] + dp[i - 3]) % MOD;
+    }
+
+    return dp[n]
+};
