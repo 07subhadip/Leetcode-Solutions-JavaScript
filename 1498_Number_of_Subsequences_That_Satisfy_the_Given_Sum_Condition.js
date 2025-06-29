7b71caa2fa4ce00b1c81b1f3a19884d23967d7ce
07subhadip <subhadiphensh31102000@gmail.com>
2025-06-29 22:54:34 +0530

    feat: add numSubseq function to calculate the number of subsequences that satisfy the given sum condition

---
+++ 1498_Number_of_Subsequences_That_Satisfy_the_Given_Sum_Condition.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var numSubseq = function (nums, target) {
+    nums.sort((a, b) => a - b)
+    const mod = 1e9 + 7
+    const n = nums.length
+    const f = Array(n + 1).fill(1)
+    for (let i = 1; i <= n; ++i) {
+        f[i] = (f[i - 1] * 2) % mod
+    }
+
+    let ans = 0
+    for (let i = 0; i < n && nums[i] * 2 <= target; ++i) {
+        const j = search(nums, target - nums[i], i + 1) - 1
+        if (j >= i) {
+            ans = (ans + f[j - i]) % mod
+        }
+    }
+    return ans
+};
+
+function search(nums, x, left) {
+    let right = nums.length
+    while (left < right) {
+        const mid = (left + right) >> 1
+        if (nums[mid] > x) {
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+    return left
+}
