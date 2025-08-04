5e5f35020b02a3742678baa7f580f5ccb1ca39ff
07subhadip <subhadiphensh31102000@gmail.com>
2025-08-04 09:49:44 +0530

    feat: implement solution for LeetCode 904 - Fruit Into Baskets

---
+++ 904_Fruit_Into_Baskets.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+var totalFruit = function (fruits) {
+    const n = fruits.length
+    const cnt = new Map()
+    let ans = 0
+    for (let i = 0, j = 0; i < n; ++i) {
+        cnt.set(fruits[i], (cnt.get(fruits[i]) || 0) + 1)
+        for (; cnt.size > 2; ++j) {
+            cnt.set(fruits[j], cnt.get(fruits[j]) - 1)
+            if (!cnt.get(fruits[j])) {
+                cnt.delete(fruits[j])
+            }
+        }
+        ans = Math.max(ans, i - j + 1)
+    }
+    return ans
+};
