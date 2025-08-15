31a7817524b5a35b57399e6e2ff3634027122e5d
07subhadip <subhadiphensh31102000@gmail.com>
2025-08-15 22:51:45 +0530

    feat: add optimized solution for LeetCode 342 - Power of Four

--- 342_Power_of_Four.js
+++ 342_Power_of_Four.js
@@ -18,3 +18,13 @@ var isPowerOfFour = function (n) {
         }
     }
 };
+
+// Second solution most optimized and efficient
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function (n) {
+    return n > 0 && (n & (n - 1)) === 0 && (n & 0x55555555) !== 0;
+};
