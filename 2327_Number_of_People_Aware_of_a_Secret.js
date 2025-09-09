32305b8e9c6a7c331b0d6c4b3d57cf9b2b1e15aa
07subhadip <subhadiphensh31102000@gmail.com>
2025-09-09 22:44:27 +0530

    feat: add solution for LeetCode 2327 - Number of People Aware of a Secret

---
+++ 2327_Number_of_People_Aware_of_a_Secret.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} n
+ * @param {number} delay
+ * @param {number} forget
+ * @return {number}
+ */
+var peopleAwareOfSecret = function (n, delay, forget) {
+    const mod = 1e9 + 7;
+    const m = (n << 1) + 10;
+    const d = Array(m).fill(0);
+    const cnt = Array(m).fill(0);
+
+    cnt[1] = 1;
+    for (let i = 1; i <= n; ++i) {
+        if (cnt[i] > 0) {
+            d[i] = (d[i] + cnt[i]) % mod;
+            d[i + forget] = (d[i + forget] - cnt[i] + mod) % mod;
+            let nxt = i + delay;
+            while (nxt < i + forget) {
+                cnt[nxt] = (cnt[nxt] + cnt[i]) % mod;
+                ++nxt;
+            }
+        }
+    }
+
+    let ans = 0;
+    for (let i = 1; i <= n; ++i) {
+        ans = (ans + d[i]) % mod;
+    }
+    return ans;
+};
