013cafe076d9802d7f41bd6450ff9cd79177d51b
07subhadip <subhadiphensh31102000@gmail.com>
2025-09-23 00:00:00 +0530

    Solve 165_Compare_Version_Numbers.js

---
+++ 165_Compare_Version_Numbers.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} version1
+ * @param {string} version2
+ * @return {number}
+ */
+var compareVersion = function (version1, version2) {
+    const [m, n] = [version1.length, version2.length];
+    let [i, j] = [0, 0];
+    while (i < m || j < n) {
+        let [a, b] = [0, 0];
+        while (i < m && version1[i] !== '.') {
+            a = a * 10 + +version1[i];
+            i++;
+        }
+        while (j < n && version2[j] !== '.') {
+            b = b * 10 + +version2[j];
+            j++;
+        }
+        if (a !== b) {
+            return a < b ? -1 : 1;
+        }
+        i++;
+        j++;
+    }
+    return 0;
+};
