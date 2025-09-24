ceddb432c30dbf2985c02e47cb52c6268057ab9f
07subhadip <subhadiphensh31102000@gmail.com>
2025-09-25 00:00:00 +0530

    Solve 120_Triangle.js

---
+++ 120_Triangle.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+var minimumTotal = function(triangle) {
+    const n = triangle.length;
+    const f = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0));
+    for (let i = n - 1; i >= 0; --i) {
+        for (let j = 0; j <= i; ++j) {
+            f[i][j] = Math.min(f[i + 1][j], f[i + 1][j + 1]) + triangle[i][j];
+        }
+    }
+    return f[0][0];
+};
