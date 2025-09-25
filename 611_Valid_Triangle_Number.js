8052f0791e072161310ec30e31a98ff3273aac60
07subhadip <subhadiphensh31102000@gmail.com>
2025-09-26 00:00:00 +0530

    Solve 611_Valid_Triangle_Number.js

---
+++ 611_Valid_Triangle_Number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var triangleNumber = function (nums) {
+    nums.sort((a, b) => a - b);
+    const n = nums.length;
+    let ans = 0;
+    for (let i = 0; i < n - 2; i++) {
+        for (let j = i + 1; j < n - 1; j++) {
+            const sum = nums[i] + nums[j];
+            let k = _.sortedIndex(nums, sum, j + 1) - 1;
+            if (k > j) {
+                ans += k - j;
+            }
+        }
+    }
+    return ans;
+};
