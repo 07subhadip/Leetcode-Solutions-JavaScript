bf02361882c944a1724964a36fed181f9436b9af
07subhadip <subhadiphensh31102000@gmail.com>
2025-10-14 07:03:26 +0530

    Add solution for 3349_Adjacent_Increasing_Subarrays_Detection_I

---
+++ 3349_Adjacent_Increasing_Subarrays_Detection_I.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var hasIncreasingSubarrays = function (nums, k) {
+    const n = nums.length;
+
+    for (let i = 0; i <= n - 2 * k; i++) {
+        let firstIncreasing = true;
+        for (let j = i; j < i + k - 1; j++) {
+            if (nums[j] >= nums[j + 1]) {
+                firstIncreasing = false;
+                break;
+            }
+        }
+
+        if (!firstIncreasing) continue;
+
+        let secondIncreasing = true;
+        for (let j = i + k; j < i + 2 * k - 1; j++) {
+            if (nums[j] >= nums[j + 1]) {
+                secondIncreasing = false;
+                break;
+            }
+        }
+
+        if (firstIncreasing && secondIncreasing) {
+            return true;
+        }
+    }
+
+    return false;
+};
