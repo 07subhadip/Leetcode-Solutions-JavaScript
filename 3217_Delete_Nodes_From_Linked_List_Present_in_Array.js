aebf26668c477094ffd4c96e499d39c45612883d
07subhadip <subhadiphensh31102000@gmail.com>
2025-11-01 23:34:48 +0530

    refactor: clean up whitespace and improve formatting in modifiedList function

--- 3217_Delete_Nodes_From_Linked_List_Present_in_Array.js
+++ 3217_Delete_Nodes_From_Linked_List_Present_in_Array.js
@@ -10,11 +10,11 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var modifiedList = function(nums, head) {
+var modifiedList = function (nums, head) {
     const s = new Set(nums);
     const dummy = new ListNode(0, head);
     let pre = dummy;
-    
+
     while (pre.next) {
         if (s.has(pre.next.val)) {
             pre.next = pre.next.next;
@@ -22,6 +22,40 @@ var modifiedList = function(nums, head) {
             pre = pre.next;
         }
     }
-    
+
+    return dummy.next;
+};
+
+
+
+/*-----------------------------------------------------*/
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var modifiedList = function (nums, head) {
+    const numSet = new Set(nums);
+
+    const dummy = new ListNode(0, head);
+    let current = dummy;
+
+    while (current.next !== null) {
+        if (numSet.has(current.next.val)) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+
     return dummy.next;
 };
