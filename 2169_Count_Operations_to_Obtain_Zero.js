1f7afa48fcc51ca4fdbbbee97fc54e48c1cbce0f
07subhadip <subhadiphensh31102000@gmail.com>
2025-11-09 12:11:20 +0530

    feat: implement countOperations function to calculate the number of operations to make two integers zero

---
+++ 2169_Count_Operations_to_Obtain_Zero.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var countOperations = function (num1, num2) {
+    let res = 0
+
+    while (num1 && num2) {
+        if (num1 >= num2) {
+            res += Math.floor(num1 / num2)
+            num1 = num1 % num2
+        } else {
+            res += Math.floor(num2 / num1)
+            num2 = num2 % num1
+        }
+    }
+
+    return res
+};
