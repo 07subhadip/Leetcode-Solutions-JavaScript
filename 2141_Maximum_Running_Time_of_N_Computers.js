fb6e5e348c918d8263547c487949a46ff31b25ea
07subhadip <subhadiphensh31102000@gmail.com>
2025-12-01 22:07:57 +0530

    feat: implement maxRunTime function to calculate the maximum running time of N computers

---
+++ 2141_Maximum_Running_Time_of_N_Computers.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @param {number[]} batteries
+ * @return {number}
+ */
+var maxRunTime = function (n, batteries) {
+    let l = 0n;
+    let r = 0n;
+    for (const x of batteries) {
+        r += BigInt(x);
+    }
+    while (l < r) {
+        const mid = (l + r + 1n) >> 1n;
+        let s = 0n;
+        for (const x of batteries) {
+            s += BigInt(Math.min(x, Number(mid)));
+        }
+        if (s >= mid * BigInt(n)) {
+            l = mid;
+        } else {
+            r = mid - 1n;
+        }
+    }
+    return Number(l);
+};
