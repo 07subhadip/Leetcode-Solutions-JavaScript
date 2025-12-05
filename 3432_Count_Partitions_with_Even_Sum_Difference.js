fd0ad82ee9abe578dafb7586e1f7db8283a3f889
07subhadip <subhadiphensh31102000@gmail.com>
2025-12-05 23:28:57 +0530

    feat: Refactor countPartitions function and add alternative solution

--- 3432_Count_Partitions_with_Even_Sum_Difference.js
+++ 3432_Count_Partitions_with_Even_Sum_Difference.js
@@ -2,7 +2,27 @@
  * @param {number[]} nums
  * @return {number}
  */
-var countPartitions = function(nums) {
+var countPartitions = function (nums) {
     const total = nums.reduce((acc, val) => acc + val, 0);
     return total % 2 === 0 ? nums.length - 1 : 0;
+};
+
+
+// Solution 2
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countPartitions = function (nums) {
+    let totalSum = 0;
+    for (let num of nums) {
+        totalSum += num;
+    }
+
+    if (totalSum % 2 !== 0) {
+        return 0;
+    }
+
+    return nums.length - 1;
 };
