cac1f37d7b7a426841292885f289a4852dd73260
Subhadip Hensh <subhadiphensh31102000@gmail.com>
2025-12-18 23:49:00 +0530

    Add 3562_Maximum_Profit_from_Trading_Stocks_with_Discounts.js

---
+++ 3562_Maximum_Profit_from_Trading_Stocks_with_Discounts.js
@@ -0,0 +1,49 @@
+/**
+ * @param {number} n
+ * @param {number[]} present
+ * @param {number[]} future
+ * @param {number[][]} hierarchy
+ * @param {number} budget
+ * @return {number}
+ */
+var maxProfit = function (n, present, future, hierarchy, budget) {
+    const g = Array.from({ length: n + 1 }, () => []);
+
+    for (const [u, v] of hierarchy) {
+        g[u].push(v);
+    }
+
+    const dfs = (u) => {
+        const nxt = Array.from({ length: budget + 1 }, () => [0, 0]);
+
+        for (const v of g[u]) {
+            const fv = dfs(v);
+            for (let j = budget; j >= 0; j--) {
+                for (let jv = 0; jv <= j; jv++) {
+                    for (let pre = 0; pre < 2; pre++) {
+                        nxt[j][pre] = Math.max(nxt[j][pre], nxt[j - jv][pre] + fv[jv][pre]);
+                    }
+                }
+            }
+        }
+
+        const f = Array.from({ length: budget + 1 }, () => [0, 0]);
+        const price = future[u - 1];
+
+        for (let j = 0; j <= budget; j++) {
+            for (let pre = 0; pre < 2; pre++) {
+                const cost = Math.floor(present[u - 1] / (pre + 1));
+                if (j >= cost) {
+                    const profitIfBuy = nxt[j - cost][1] + (price - cost);
+                    f[j][pre] = Math.max(nxt[j][0], profitIfBuy);
+                } else {
+                    f[j][pre] = nxt[j][0];
+                }
+            }
+        }
+
+        return f;
+    };
+
+    return dfs(1)[budget][0];
+};
