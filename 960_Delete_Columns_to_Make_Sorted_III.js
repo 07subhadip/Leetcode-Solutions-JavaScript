4a2ffdde68abdb9c57ce700fde5b0bcb7477ffa2
Subhadip Hensh <subhadiphensh31102000@gmail.com>
2025-12-22 23:37:19 +0530

    feat: Add LC Problem 960 Delete Columns to Make Sorted III

---
+++ 960_Delete_Columns_to_Make_Sorted_III.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} strs
+ * @return {number}
+ */
+var minDeletionSize = function (strs) {
+    const n = strs[0].length;
+    const f = Array(n).fill(1);
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            let ok = true;
+            for (const s of strs) {
+                if (s[j] > s[i]) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok) {
+                f[i] = Math.max(f[i], f[j] + 1);
+            }
+        }
+    }
+    return n - Math.max(...f);
+};
