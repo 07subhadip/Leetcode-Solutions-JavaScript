b376c5e6314b5941c97f4f3d9fea0e4c4d4d2215
Subhadip Hensh <subhadiphensh31102000@gmail.com>
2025-12-26 21:59:28 +0530

    feat: Add JavaScript solution for problem 2483 Minimum Penalty for a Shop.

---
+++ 2483_Minimum_Penalty_for_a_Shop.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} customers
+ * @return {number}
+ */
+var bestClosingTime = function (customers) {
+    let ans = 0;
+    let cost = 0;
+    for (const ch of customers) {
+        if (ch === 'Y') {
+            cost++;
+        }
+    }
+    let mn = cost;
+
+    for (let j = 1; j <= customers.length; j++) {
+        const c = customers[j - 1];
+        cost += c === 'N' ? 1 : -1;
+        if (cost < mn) {
+            mn = cost;
+            ans = j;
+        }
+    }
+    return ans;
+};
