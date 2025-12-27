009f0b2cf6c9eccadc892b85303ff749552b7aac
Subhadip Hensh <subhadiphensh31102000@gmail.com>
2025-12-27 10:47:07 +0530

    feat: add a second solution for the Meeting Rooms III problem

--- 2402_Meeting_Rooms_III.js
+++ 2402_Meeting_Rooms_III.js
@@ -67,3 +67,52 @@ var mostBooked = function (n, meetings) {
     }
     return ans;
 };
+
+
+// Solution 2:
+
+/**
+ * @param {number} n
+ * @param {number[][]} meetings
+ * @return {number}
+ */
+var mostBooked = function (n, meetings) {
+    meetings.sort((a, b) => a[0] - b[0]);
+
+    const idle = new MinPriorityQueue();
+    for (let i = 0; i < n; ++i) {
+        idle.enqueue(i);
+    }
+    const busy = new PriorityQueue((a, b) => {
+        if (a[0] === b[0]) {
+            return a[1] - b[1];
+        }
+        return a[0] - b[0];
+    });
+    const cnt = new Array(n).fill(0);
+    for (const v of meetings) {
+        const s = v[0],
+            e = v[1];
+        while (!busy.isEmpty() && busy.front()[0] <= s) {
+            const i = busy.dequeue()[1];
+            idle.enqueue(i);
+        }
+        let i = 0;
+        if (!idle.isEmpty()) {
+            i = idle.dequeue();
+            busy.enqueue([e, i]);
+        } else {
+            const x = busy.dequeue();
+            i = x[1];
+            busy.enqueue([x[0] + e - s, i]);
+        }
+        ++cnt[i];
+    }
+    let ans = 0;
+    for (let i = 0; i < n; ++i) {
+        if (cnt[ans] < cnt[i]) {
+            ans = i;
+        }
+    }
+    return ans;
+};
