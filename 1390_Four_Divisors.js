5fd121cdd9bdf63bb8c5d60dc81e69570e025197
Subhadip Hensh <subhadiphensh31102000@gmail.com>
2026-01-04 19:04:26 +0530

    feat: add solution for LeetCode 1390 Four Divisors

---
+++ 1390_Four_Divisors.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumFourDivisors = function (nums) {
+    const f = (x) => {
+        let cnt = 2;
+        let s = x + 1;
+        for (let i = 2; i * i <= x; ++i) {
+            if (x % i === 0) {
+                ++cnt;
+                s += i;
+                if (i * i !== x) {
+                    ++cnt;
+                    s += Math.floor(x / i);
+                }
+            }
+        }
+        return cnt === 4 ? s : 0;
+    };
+    return nums.reduce((acc, x) => acc + f(x), 0);
+};
