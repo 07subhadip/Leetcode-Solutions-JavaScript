6cda0ecb977698e497f3f3d708f71a0585203412
Subhadip Hensh <subhadiphensh31102000@gmail.com>
2026-01-05 23:06:17 +0530

    refactor: replace maxMatrixSum implementation with a new, single-pass solution

--- 1975_Maximum_Matrix_Sum.js
+++ 1975_Maximum_Matrix_Sum.js
@@ -2,7 +2,7 @@
  * @param {number[][]} matrix
  * @return {number}
  */
-var maxMatrixSum = function(matrix) {
+var maxMatrixSum = function (matrix) {
     let cnt = 0;
     let s = 0;
     let mi = Infinity;
@@ -17,4 +17,25 @@ var maxMatrixSum = function(matrix) {
         return s;
     }
     return s - mi * 2;
+};
+
+// Solution 2
+
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var maxMatrixSum = function (matrix) {
+    let [s, cnt, mi] = [0, 0, Infinity];
+    for (const row of matrix) {
+        for (const x of row) {
+            if (x < 0) {
+                ++cnt;
+            }
+            const y = Math.abs(x);
+            s += y;
+            mi = Math.min(mi, y);
+        }
+    }
+    return cnt % 2 === 0 ? s : s - 2 * mi;
 };
