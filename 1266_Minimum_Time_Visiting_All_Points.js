30cd2719c96aeabeecb8597081f5804c5d9cb6ec
Subhadip Hensh <subhadiphensh31102000@gmail.com>
2026-01-12 10:07:39 +0530

    Add solution for LeetCode 1266: Minimum Time Visiting All Points

---
+++ 1266_Minimum_Time_Visiting_All_Points.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var minTimeToVisitAllPoints = function (points) {
+    let total = 0;
+    for (let i = 1; i < points.length; i++) {
+        const dx = Math.abs(points[i][0] - points[i - 1][0]);
+        const dy = Math.abs(points[i][1] - points[i - 1][1]);
+        total += Math.max(dx, dy);
+    }
+    return total;
+};
