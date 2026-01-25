4a1dc3da02459bbae4f22ef33b70d7f5f3d49957
Subhadip Hensh <subhadiphensh31102000@gmail.com>
2026-01-25 17:57:29 +0530

    feat: Add solution for LeetCode 1984 Minimum Difference Between Highest and Lowest of K Scores.

---
+++ 1984_Minimum_Difference_Between_Highest_and_Lowest_of_K_Scores.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minimumDifference = function (nums, k) {
+    nums.sort((a, b) => a - b);
+    const n = nums.length;
+    let ans = nums[n - 1] - nums[0];
+    for (let i = 0; i + k - 1 < n; i++) {
+        ans = Math.min(nums[i + k - 1] - nums[i], ans);
+    }
+    return ans;
+};
