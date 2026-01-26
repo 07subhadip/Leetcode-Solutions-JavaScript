1dfe18665f8249dc96af5dc540d1efea9685ddf1
Subhadip Hensh <subhadiphensh31102000@gmail.com>
2026-01-26 12:00:00 +0530

    Solve 1200. Minimum Absolute Difference

---
+++ 1200_Minimum_Absolute_Difference.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {number[][]}
+ */
+var minimumAbsDifference = function (arr) {
+    arr.sort((a, b) => a - b);
+    let mi = 1 << 30;
+    const n = arr.length;
+    for (let i = 0; i < n - 1; ++i) {
+        mi = Math.min(mi, arr[i + 1] - arr[i]);
+    }
+    const ans = [];
+    for (let i = 0; i < n - 1; ++i) {
+        if (arr[i + 1] - arr[i] === mi) {
+            ans.push([arr[i], arr[i + 1]]);
+        }
+    }
+    return ans;
+};
