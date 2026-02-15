e4f97e7f244608f0a664b48255400fa6e3e9569d
Subhadip Hensh <subhadiphensh31102000@gmail.com>
2026-02-15 23:30:16 +0530

    Add solution for problem 67: Add Binary

---
+++ 67_Add_Binary.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function (a, b) {
+    let i = a.length - 1;
+    let j = b.length - 1;
+    const ans = [];
+    for (let carry = 0; i >= 0 || j >= 0 || carry; --i, --j) {
+        carry += (i >= 0 ? a[i] : '0').charCodeAt(0) - '0'.charCodeAt(0);
+        carry += (j >= 0 ? b[j] : '0').charCodeAt(0) - '0'.charCodeAt(0);
+        ans.push(carry % 2);
+        carry >>= 1;
+    }
+    return ans.reverse().join('');
+};
