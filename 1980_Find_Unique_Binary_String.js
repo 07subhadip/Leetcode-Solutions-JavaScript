aee1ad9489ffd1a089a727f6efbe60515419408e
Subhadip Hensh <subhadiphensh31102000@gmail.com>
2026-03-08 11:00:00 +0530

    Add solution to LeetCode problem 1980: Find Unique Binary String

--- 1980_Find_Unique_Binary_String.js
+++ 1980_Find_Unique_Binary_String.js
@@ -4,9 +4,36 @@
  */
 var findDifferentBinaryString = function (nums) {
     let n = nums.length;
-    let result = "";
+    let result = '';
     for (let i = 0; i < n; i++) {
-        result += nums[i][i] === "0" ? "1" : "0";
+        result += nums[i][i] === '0' ? '1' : '0';
     }
     return result;
+};
+
+// Solution 2
+
+/**
+ * @param {string[]} nums
+ * @return {string}
+ */
+var findDifferentBinaryString = function (nums) {
+    const n = nums.length;
+    const numsSet = new Set(nums);
+
+    function generate(curr) {
+        if (curr.length === n) {
+            if (!numsSet.has(curr)) {
+                return curr;
+            }
+            return '';
+        }
+
+        let addZero = generate(curr + '0');
+        if (addZero) return addZero;
+
+        return generate(curr + '1');
+    }
+
+    return generate('');
 };
